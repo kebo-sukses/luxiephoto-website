e4e8e4e97cec3679fb0af8ad2fb6930e55e83ebe
kebo-sukses <73052720+kebo-sukses@users.noreply.github.com>
2026-02-03 16:23:00 +0700

    Add FloatingWhatsApp export to common components index

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -3,3 +3,4 @@ export { default as SectionTitle } from './SectionTitle';
 export { default as OptimizedImage } from './OptimizedImage';
 export { default as AnimatedSection, StaggerContainer, StaggerItem } from './AnimatedSection';
 export { default as DynamicFavicon } from './DynamicFavicon';
+export { default as FloatingWhatsApp } from './FloatingWhatsApp';
